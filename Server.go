e5c50de5821e59af5ee4a2e3b1061cdc7e8fbf19
Lucas Frias <>
2019-04-22 17:27:59 -0700

    Client can create and join chat rooms with specified name. Also, they can leave the chat room.

--- Server.go
+++ Server.go
@@ -26,12 +26,15 @@ func (lobby *Lobby) Join(client *Client) {
 	lobby.clients = append(lobby.clients, client)
 	client.Write("Welcome to GoChat, you'll need an account to continue\n")
 	client.authenticate()
+	go lobby.start(client)
+}
+
+func (lobby *Lobby) start(client *Client) {
 	client.menu()
-	go func() {
-		for {
-			lobby.Read(client)
-		}
-	}()
+	for {
+		str, _ := client.reader.ReadString('\n')
+		lobby.ParseLobby(str, client)
+	}
 }
 
 //used to get username to test other methods
@@ -55,28 +58,51 @@ func (lobby *Lobby) CreateChatRoom(client *Client, name string) {
 	lobby.JoinChatRoom(client, name)
 }
 
-func (lobby *Lobby) JoinChatRoom(client *Client, name string) {
-	if lobby.chatRooms[name] == nil {
-		client.Write("no chat rooms with that name")
-		return
-	}
-	lobby.chatRooms[name].Join(client)
+func (client *Client) ChooseChatRoomName() string {
+	client.Write("Please pick a name for your chat room.\n")
+	name, _ := client.reader.ReadString('\n')
+	return strings.TrimSpace(name)
 }
 
-func (client *Client) ChatRoomCommands(name string) {
-	client.Write("---------- " + name + " Group " + "----------\n")
-	client.Write("Commands: ~invite  #Invite a user\n" +
-		"  \t\t~leave #Leave the group\n" +
-		"  \t\t~fs #Send file\n" +
-		"  \t\t~dwnld #Download file\n")
+func (lobby *Lobby) JoinChatRoom(client *Client, name string) {
+	if lobby.chatRooms[name] == nil {
+		client.Write("Incorrect. Try another name.\n")
+		lobby.ParseLobby("2", client) //sends second option back to the menu so user can try again
+	}
+
+	lobby.chatRooms[name].Join(client, lobby)
+	client.chatRoom.Send(client.UserName + " has joined.")
+}
+
+func (lobby *Lobby) AvailableChatRooms(client *Client) {
+	client.Write("Available Chat Rooms:\n")
+	for name := range lobby.chatRooms {
+		client.Write(name)
+		client.Write("\n")
+	}
+}
+
+func (lobby *Lobby) getName(client *Client) string {
+	client.Write("Please choose a chat room.\n")
+	name, _ := client.reader.ReadString('\n')
+	return strings.TrimSpace(name)
 }
 
 func (lobby *Lobby) ParseLobby(message string, client *Client) {
 	switch {
 	case strings.TrimSpace(message) == "1":
-		lobby.CreateChatRoom(client, "Test")
+		name := client.ChooseChatRoomName()
+		lobby.CreateChatRoom(client, name)
 	case strings.TrimSpace(message) == "2":
-		lobby.JoinChatRoom(client, "Test")
+		if len(lobby.chatRooms) == 0 {
+			client.Write("No chat rooms.\n")
+			client.Write("Choose a different option.\n")
+		} else {
+			lobby.AvailableChatRooms(client)
+			name := lobby.getName(client)
+			lobby.JoinChatRoom(client, name)
+			client.ChatRoomCommands(client.chatRoom.name)
+		}
 	case strings.TrimSpace(message) == "3":
 		//view invites
 	default:
@@ -95,6 +121,7 @@ type ChatRoom struct {
 	name     string
 	clients  []*Client
 	messages []string
+	invites  []string
 }
 
 func NewChatRoom(name string) *ChatRoom {
@@ -102,27 +129,50 @@ func NewChatRoom(name string) *ChatRoom {
 		name:     name,
 		clients:  make([]*Client, 0),
 		messages: make([]string, 0),
+		invites:  make([]string, 0),
 	}
 }
 
-func (chatRoom *ChatRoom) Join(client *Client) {
+func (client *Client) ChatRoomCommands(name string) {
+	client.Write("---------- " + name + " Group " + "----------\n")
+	client.Write("Commands: ~invite  #Invite a user\n" +
+		"  \t\t~leave #Leave the group\n" +
+		"  \t\t~fs #Send file\n" +
+		"  \t\t~dwnld #Download file\n")
+}
+
+func (chatRoom *ChatRoom) Join(client *Client, lobby *Lobby) {
 	client.chatRoom = chatRoom
 	for _, message := range chatRoom.messages {
 		client.Write(message)
 	}
 	chatRoom.clients = append(chatRoom.clients, client)
 	for {
-		ReadChatRoom(client)
+		chatRoom.ReadChatRoom(client, lobby)
 	}
 
 }
 
-func ParseChatRoom(message string, client *Client) {
+func (chatRoom *ChatRoom) ReadChatRoom(client *Client, lobby *Lobby) {
+	for {
+		str, _ := client.reader.ReadString('\n')
+		chatRoom.ParseChatRoom(str, client, lobby)
+	}
+}
+
+func (chatRoom *ChatRoom) LeaveChatRoom(client *Client, lobby *Lobby) {
+	client.chatRoom.Send(client.UserName + " is leaving the chat room.\n")
+	client.chatRoom = nil
+	lobby.start(client)
+}
+
+func (chatRoom *ChatRoom) ParseChatRoom(message string, client *Client, lobby *Lobby) {
 	switch {
 	case strings.HasPrefix(message, "~invite"):
-		//invite
-	case strings.HasPrefix(message, "~leave"):
-		//leave
+		client.Write("test\n")
+	case strings.TrimSpace(message) == "~leave":
+		client.Write("\n")
+		chatRoom.LeaveChatRoom(client, lobby)
 	case strings.HasPrefix(message, "~fs"):
 		//send file
 	case strings.HasPrefix(message, "~dwnld"):
@@ -140,6 +190,15 @@ func (chatRoom *ChatRoom) Send(message string) {
 		client.Write(message)
 	}
 }
+
+/*func (lobby *Lobby)Invite(person string, chatRoom *ChatRoom, client *Client){
+	client.Write("All connected clients:\n")
+	for p := range lobby.clients {
+		client.Write(p)
+		client.Write("\n")
+	}
+}
+*/
 
 type Client struct {
 	UserName string
@@ -161,13 +220,6 @@ func NewClient(conn net.Conn) *Client {
 		writer:   writer,
 	}
 	return client
-}
-
-func ReadChatRoom(client *Client) {
-	for {
-		str, _ := client.reader.ReadString('\n')
-		ParseChatRoom(str, client)
-	}
 }
 
 func (client *Client) Write(str string) {
